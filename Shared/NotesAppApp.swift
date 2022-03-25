ebbdfb39d617390804d248f68db128501db17b72
jina <psalvarajah@lcs.on.ca>
2022-03-25 11:35:15 -0400

    Initial Commit

---
+++ Shared/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  Shared
+//
+//  Created by Salvarajah, Prajina on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
